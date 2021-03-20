76264773d1f2b35a05724afd0c9d18c28c1daf95
priya-nusrat18 <nusratjahan5552@gmail.com>
2021-03-20 23:04:00 +0600

    riding club website

---
+++ src/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlkRQudvzzIKw3hOMCWD-85_3NFr-SFWs",
+    authDomain: "bhyuiyan-s-gokid.firebaseapp.com",
+    projectId: "bhyuiyan-s-gokid",
+    storageBucket: "bhyuiyan-s-gokid.appspot.com",
+    messagingSenderId: "180220832034",
+    appId: "1:180220832034:web:aeeb4d6e8bfa0bc1a75d7b"
+  };
+  export default firebaseConfig;
